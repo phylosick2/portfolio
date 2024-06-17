27b42eb55757bc09c0f73b71bcc403ebd3f06a11
phylosick <garvitkhare2004@gmail.com>
2024-06-17 12:22:43 +0530

    changes done

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,9 +8,9 @@ export const Bio = {
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/phylosick",
+  github: "https://github.com/phylosick2",
   resume:
-    "https://drive.google.com/file/d/1XlS0YUMwl8Kv0owcPP9Nba_Vfasl4-Mq/view?usp=drive_link",
+    "https://drive.google.com/file/d/1hvjlCja5Q6ssPbVO7UzAI0VnlooyT2pE/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/garvit-khare-2297a924b/?originalSubdomain=in",
   twitter: "",
   insta: "https://www.instagram.com/g.a.r.v.i.t._.09/",
